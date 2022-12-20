579c634f9975ef0d32832bdf65a9a94a2150f1e2
aefe2 <shigaev_1990@mail.ru>
2022-12-20 13:32:25 +0700

    tutyu

--- exam/main/urls.py
+++ exam/main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from main.views import indexview, services, about, contacts
+from main.views import indexview, about, contacts, service_create, service_render
 
 urlpatterns = [
     path('', indexview, name='index'),
-    path('services/', services, name='services'),
+    path('services/', service_render, name='services'),
     path('about/', about, name='about'),
     path('contacts/', contacts, name='contacts'),
+    path('service_add/', service_create, name='service_add'),
 ]
